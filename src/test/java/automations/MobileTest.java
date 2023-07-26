9218c84e88efe9da53f362089953e0762014979e
sharumathpriya@gmail.com <sharumathpriya@gmail.com>
2023-07-26 01:42:42 -0700

    initial commit

---
+++ src/test/java/automations/MobileTest.java
@@ -0,0 +1,19 @@
+package automations;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+	@Test
+	void login()
+	{
+		System.out.println("Login");
+	}
+	@Test
+	
+	void logout()
+	{
+		System.out.println("Logout");
+		//int num=10/0;
+	}
+
+}
